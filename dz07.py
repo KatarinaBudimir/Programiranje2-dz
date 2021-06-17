389a22aeff57efeb37f2f77d1de0d58309e90334
KatarinaBudimir <85709995+KatarinaBudimir@users.noreply.github.com>
2021-06-17 22:27:50 +0200

    Delete dz07.py

--- dz07.py
+++
@@ -1,8 +0,0 @@
-'''Funkciju iz prethodne zadaće učitati kao funkciju modula u
-novi program i pozvati je nakon traženja unosa od korisnika.'''
-
-import dz6_rekurzija
-
-unos=input("Unesite string ili recenicu: ")
-
-print(dz6_rekurzija.string_rekurzija(unos))
